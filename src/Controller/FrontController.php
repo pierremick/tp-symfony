b45f9250679f07544636b3016f6843865153c3a0
Pierre Mick <pierre@mick.fr>
2023-02-15 10:52:38 +0100

    update

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\PositionFilterType;
 use App\Repository\PositionRepository;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
