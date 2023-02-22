32fa6203adb53afd0fb5deb685ef18428f331107
Pierre Mick <pierre@mick.fr>
2023-02-22 14:12:52 +0100

    update owner

--- src/Controller/OwnerController.php
+++ src/Controller/OwnerController.php
@@ -20,13 +20,12 @@ class OwnerController extends AbstractController
     #[Route('/account/owner', name: 'account_owner')]
     public function account_owner(Request $request, UserRepository $userRepository): Response
     {
-        // Récupère les positions actives à afficher
-        $users = $userRepository->findOwnersWithPositions();
+        $user = $this->getUser(); // Récupère l'utilisateur connecté
+        $positions = $positionRepository->findByOwner($user);
 
-        // Envoi la réponse à la vue dans le template twig 'front/archive_position.html.twig'
-        return $this->render('account/owner/index.html.twig', [
-            'page_name' => 'proprietaire',
-            'page_title' => "Propriétaire",
+        return $this->render('account/index.html.twig', [
+            'page_title' => 'Mon compte',
+            'positions' => $positions,
             'users' => $users,
         ]);
     }
